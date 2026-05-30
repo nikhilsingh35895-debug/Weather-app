5e9d4144c82bae69aed3020c462a4c8f9d948f27
nikhilsingh35895 <nikhilsingh35895>
2026-05-30 19:08:24 +0530

    Third Commit

--- weather.js
+++ weather.js
@@ -13,6 +13,55 @@ searchBtn.addEventListener("click", async() => {
     }
 
        try{
+        resultBox.classList.remove("hidden");
+        resultBox.innerHTML = `
+        
+          <div class="flex justify-center items-center gap-2 ">
+
+    <div class="w-10 h-10 border-3 border-blue-500 border-t-transparent rounded-full animate-spin  ">
+        </div>
+        <p class="text-center mt-3 ms-1 text-gray-700 font-medium ">Fetching  Weather Data..</p>
+
+</div> `;
+
+   let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apikey}&units=metric`,);
+
+   if(!response.ok){
+    resultBox.innerHTML =`
+    <h3>city not found.....</h3>
+    `;
+    return;
+   }
+
+   let data = await response.json();
+   console.log(data);
+
+   let icon = data.weather[0].icon;
+
+   resultBox.innerHTML = `
+
+   <div>
+    <h2 class="text-3xl font-bold text-gray-800  ">${data.name}, ${data.sys.country}</h2>
+    <p class="text-gray-600 mt-2  "> ${data.weather[0].main}</p>
+  <img src="https://openweathermap.org/img/wn/${icon}@2x.png" alt="loading.. " class="mx-auto">
+  <h1 class="text-5xl font-bold text-blue-700 ">${data.main.temp}°C</h1>
+</div>
+
+<div class="grid grid-cols-2 gap-4 mt-6  ">
+    <div class="bg-white/40 p-4 rounded-xl shadow ">
+        <p class="text-gray-600 text-sm">Humidity</p>
+        <h3 class="text-2xl font-bold text-gray-800 ">${data.main.humidity}%</h3>
+
+    </div>
+
+    <div class="bg-white/60 p-4 rounded-xl shadow ">
+        <p class="text-gray-600 text-sm">wind Speed</p>
+        <h3 class="text-2xl font-bold text-gray-800 ">${data.wind.speed}%</h3>
+
+    </div>
+
+</div>`;
+        
 
        }catch (error){
         console.log(error);
